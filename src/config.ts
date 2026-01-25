4df99b6f597bdb0f53a1abc124e98ed4771280a3
nilh2a2 <nilh2a2@users.noreply.github.com>
2026-01-25 14:30:51 +0800

    remove unused features

--- src/config.ts
+++ src/config.ts
@@ -11,10 +11,6 @@ export const SITE = {
   scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
   showArchives: true,
   showBackButton: true, // show back button in post detail
-  editPost: {
-    enabled: true,
-    url: "https://github.com/satnaing/astro-paper/edit/main/",
-  },
   dynamicOgImage: true,
   dir: "ltr", // "rtl" | "auto"
   lang: "en", // html lang code. Set this empty and default will be "en"
--- src/content.config.ts
+++ src/content.config.ts
@@ -18,7 +18,6 @@ const blog = defineCollection({
       ogImage: image().or(z.string()).optional(),
       description: z.string(),
       canonicalURL: z.string().optional(),
-      hideEditPost: z.boolean().optional(),
       timezone: z.string().optional(),
     }),
 });
